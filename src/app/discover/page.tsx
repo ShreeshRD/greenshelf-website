3d12935e757917c79106280bfa010061999124ec
ShreeshRD <shreeshrdevi2@gmail.com>
2025-09-28 17:02:11 +0530

    discover page

---
+++ src/app/discover/page.tsx
@@ -0,0 +1,18 @@
+const DiscoverPage = () => {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card shadow-sm">
+          <div className="card-body p-5">
+            <h1 className="card-title text-center mb-4">Discover</h1>
+            <p className="lead text-center">
+              This is the Discover page. Content will be added soon.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DiscoverPage;
